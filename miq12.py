dab21949229bbe169e52f3263fed36adb890443f
shilorya <doknes@mail.ru>
2023-11-05 13:58:48 +0300

    Create miq12.py

---
+++ miq12.py
@@ -0,0 +1,73 @@
+# task 1(D)
+
+# z, x = input('><').split()
+
+# print(x,z)
+
+# task 2(E)
+
+# s = input('><')
+# z = s.find('f')
+# x = s.rfind('f')
+# if z == -1:
+#     print()
+# elif z == x:
+#     print(z)
+# else:
+#     print(z, x)
+
+# task 3(F)
+
+# s = input('><')
+# z = s.find('f')
+# x = s.find('f', z + 1)
+# if z == -1:
+#     print(-2)
+# elif x == -1:
+#     print(-1)
+# else:
+#     print(x)
+
+
+# task 4(G)
+
+# s = input('><')
+# s = s[:s.find('h')] + s[s.rfind('h') + 1:]
+# print(s)
+
+# task 5(X)
+
+# s = input('><')
+# c = s.index('h')
+# z = s[:c]
+# s = s[c + 1:][::-1]
+# c = s.index('h')
+# x = s[:c][:: -1]
+# s = s[c +1:][:: -1]
+# print(z + s * 2 + x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
